b730dccfaf888aa1180774b5f318f82b9cafb508
Joseph Ajigboteso <111159000+JosephAjigboteso@users.noreply.github.com>
2025-06-05 23:24:52 +0100

    fixed api error message

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 //Chaging Background
-window.addEventListener('load', changeBackground(document.body));
+window.addEventListener('load', ()=> changeBackground(document.body));
+
+//DOM Elements
+const weatherSection = document.querySelector('.weather-details');
+const getBtn = document.getElementById('getBtn');
+const acceptBtn = document.getElementById('accept-btn');
+const defaultBtn = document.getElementById('default-btn');
+const saveBtn = document.getElementById('save-btn');
+const city = document.querySelector('.city'); 
+const date = document.querySelector('.date'); 
+const weatherIcon = document.querySelector('.weather-icon');
+const tempDisplay = document.querySelector('.temp-display');
+const tempReview = document.querySelector('.temp-review');
+const windSpeed = document.querySelector('.wind-speed');
+const unitBtn = document.getElementById('unitBtn');
+const nameOfCity = document.getElementById('nameOfCity');
+const reloadBtn = document.querySelector('.reload-btn');
+
 
 window.addEventListener('load', function(){
     const hasSeenPopup = sessionStorage.getItem('hasSeenPopup');
@@ -31,21 +48,6 @@ setInterval(() => {
 }, 3000);
 }
 
-//DOM Elements
-const weatherSection = document.querySelector('.weather-details');
-const getBtn = document.getElementById('getBtn');
-const acceptBtn = document.getElementById('accept-btn');
-const defaultBtn = document.getElementById('default-btn');
-const saveBtn = document.getElementById('save-btn');
-const city = document.querySelector('.city'); 
-const date = document.querySelector('.date'); 
-const weatherIcon = document.querySelector('.weather-icon');
-const tempDisplay = document.querySelector('.temp-display');
-const tempReview = document.querySelector('.temp-review');
-const windSpeed = document.querySelector('.wind-speed');
-const unitBtn = document.getElementById('unitBtn');
-const nameOfCity = document.getElementById('nameOfCity');
-const reloadBtn = document.querySelector('.reload-btn');
 
 nameOfCity.disabled = true;
 getBtn.disabled = true;
@@ -90,7 +92,7 @@ defaultBtn.addEventListener('click', function(){
     saveBtn.style.display = 'block';
 })
 
-//save defualt city to local storage
+//save default city to local storage
 saveBtn.addEventListener('click', function(){
      let defaultCity = document.getElementById('nameOfCity').value.trim();
       if (defaultCity) {
@@ -107,12 +109,30 @@ saveBtn.addEventListener('click', function(){
 function getWeatherUpdate (cityName){
     if (!cityName) return;
      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f00c38e0279b7bc85480c3fe775d518c&units=${unit}`)
-    .then((response) => response.json())
+    .then((response) => {
+        if(!response.ok){
+              if (response.status === 404) {
+                    throw new Error("City not found");
+                } else {
+                    throw new Error("Server error");
+                }
+        }
+        return response.json();
+    })
     .then((data) => {
         removeLoadingEffect();
         displayWeather(data);
     })
-    .catch(() => handleError())
+    .catch(error =>{
+        if(error.message === 'City not found'){
+             handleError('City not found. Please check the name.');
+        }else if (error.message === "Server error") {
+                handleError("Something went wrong with the server.");
+            } else {
+                // Likely a network error (fetch didn't complete)
+                handleError("Network error. Please check your connection.");
+            }
+    })
 }
 
 //Toggling unit button
@@ -168,20 +188,20 @@ function displayWeather(data) {
    unitBtn.style.opacity = '1';
     unitBtn.style.pointerEvents = 'auto';
 }
-function handleError (){
- let nameOfCityValue = document.getElementById('nameOfCity').value.trim()
-city.innerText =`Could not get weather for ${nameOfCityValue}`;
-city.style.color = 'red';
-city.style.fontSize = '20px'
-removeData(weatherIcon)
-removeData(tempDisplay)
-removeData(tempReview)
-removeData(windSpeed)
-removeData(date)
-//disable and hide unitBtn
-unitBtn.style.opacity = '0';
-unitBtn.style.pointerEvents = 'none';
-reloadpage();
+function handleError (errorMessage){
+    removeLoadingEffect();
+    city.innerText = errorMessage;
+    city.style.color = 'red';
+    city.style.fontSize = '20px'
+    removeData(weatherIcon)
+    removeData(tempDisplay)
+    removeData(tempReview)
+    removeData(windSpeed)
+    removeData(date)
+    //disable and hide unitBtn
+    unitBtn.style.opacity = '0';
+    unitBtn.style.pointerEvents = 'none';
+    reloadpage();
 }
 
 function removeData (data){
